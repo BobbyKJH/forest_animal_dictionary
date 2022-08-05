3db6f0c5f72e34cfb8385c696ce487674cce6832
BobbyKJH <rlatnguscjsw@gmail.com>
2022-08-05 11:23:31 +0900

    Feat: ResetStyle add

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// Components
 import App from "./App";
+// Style
+import { ResetStyle } from "./style/common/ResetStyle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <React.StrictMode>
+      <ResetStyle />
       <App />
     </React.StrictMode>
   </BrowserRouter>
